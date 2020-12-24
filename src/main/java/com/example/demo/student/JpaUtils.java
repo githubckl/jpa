3e7d17b61de0372478ee0d439feadae9aac6eaaf
Z7-SP7S2 <568143444@qq.com>
2020-12-24 12:23:49 +0800

    util

--- src/main/java/com/example/demo/student/JpaUtils.java
+++ src/main/java/com/example/demo/student/JpaUtils.java
@@ -3,7 +3,7 @@ package com.example.demo.student;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+/* 实体管理器工厂的创建非常耗费资源,但是线程安全的,因此在这里做成静态的唯一对象,使它的创建只有一次却可以服务多次*/
 public class JpaUtils {
     private  static EntityManagerFactory factory;
     static{
